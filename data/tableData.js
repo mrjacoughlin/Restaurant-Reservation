537a376992be0a156bdb90c68891f27f37cdb175
James Coughlin <mrjacoughlin@gmail.com>
2020-12-08 11:56:18 -0500

    Table data Array created along with an export

---
+++ data/tableData.js
@@ -0,0 +1,13 @@
+// DATA
+// Below data will hold all of the reserved tables.
+
+var tableArray = [
+  {
+    customerName: " ",
+    customerEmail: " ",
+    customerID: " ",
+    PhoneNumber: " ",
+  },
+];
+
+module.exports = tableArray;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 let express = require("express");
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
 
+// Tells node that we are creating an "express" server
 let app = express();
+// Sets an initial port.
 let PORT = process.env.PORT || 8080;
-
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-
+// ROUTER
+// The below points our server to a series of "route" files.
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
